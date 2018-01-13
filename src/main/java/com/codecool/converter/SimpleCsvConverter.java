0b061f69bc87bd9e961e3c5bc007057c564c2e76
kamil <sirkamson@gmail.com>
2018-01-14 00:22:21 +0100

    Create csv converter class

---
+++ src/main/java/com/codecool/converter/SimpleCsvConverter.java
@@ -0,0 +1,11 @@
+package com.codecool.converter;
+
+public class SimpleCsvConverter {
+
+    FileReader fileReader = new FileReader();
+
+    public void convert(String file, OutputFormat outputFormat) {
+        System.out.println("I convert CSV to output format");
+
+    }
+}
