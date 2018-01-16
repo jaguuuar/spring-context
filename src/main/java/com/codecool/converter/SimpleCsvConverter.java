f6e245bf77b41dc57debe64b3ddabe67fcbf55a1
kamil <sirkamson@gmail.com>
2018-01-16 09:59:11 +0100

    Add bean

--- src/main/java/com/codecool/converter/SimpleCsvConverter.java
+++ src/main/java/com/codecool/converter/SimpleCsvConverter.java
@@ -1,11 +1,26 @@
 package com.codecool.converter;
 
+import com.codecool.converter.formatter.OutputFormatter;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
 public class SimpleCsvConverter {
 
-    FileReader fileReader = new FileReader();
+    private FileReader fileReader;
+    private OutputFormatterFactory outputFormatterFactory;
 
-    public void convert(String file, OutputFormat outputFormat) {
-        System.out.println("I convert csv to output format");
-
+    public SimpleCsvConverter(FileReader fileReader, OutputFormatterFactory outputFormatterFactory) {
+        this.fileReader = fileReader;
+        this.outputFormatterFactory = outputFormatterFactory;
     }
+
+    public void convert(String file, OutputFormat outputFormat) throws IOException {
+        List<String[]> data = this.fileReader.readData(file);
+        OutputFormatter outputFormatter = this.outputFormatterFactory.createByFormat(outputFormat);
+        outputFormatter.printToConsole(data);
+    }
+
 }
